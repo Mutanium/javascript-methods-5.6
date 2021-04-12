affaffa7b4351ab955a8b9e4d9e87443d4b693e5
Mutanium <info@gaetan.nl>
2021-04-12 15:30:03 +0200

    math methods toegevoegd

--- math-methods.js
+++ math-methods.js
@@ -9,16 +9,35 @@ console.log(Math.PI);
 // Tip: de formule is omtrek = diameter * pi
 // ---- Verwachte uitkomst: 12.566370614359172
 
+function omtrek (diameter) {
+    return diameter * Math.PI;
+}
+console.log(omtrek(4));
+
+// of rechtstreeks in de log:
+console.log(4*Math.PI);
+
 
 /* Opdracht 2: */
 // Bereken de omtrek van een cirkel met een diameter van 12 en rond dit af naar een heel getal. Log dit in de terminal.
 // ---- Verwachte uitkomst: 38
+
+function omtrek2 (diameter) {
+    return Math.round(diameter * Math.PI);
+}
+console.log(omtrek2(12));
+
 
 
 
 /* Opdracht 3: */
 // Bereken de wortel van 9 en log dit in de terminal
 // ---- Verwachte uitkomst: 3
+
+function sqRoot(numBer) {
+    return Math.sqrt(numBer);
+}
+console.log(sqRoot(9));
 
 
 
@@ -28,3 +47,7 @@ console.log(Math.PI);
 // * is het willekeurige getal kleiner dan 0.5 ? Log dan het getal met "Het glas is half leeg" in de terminal
 // Tip: iedere keer als je het script opnieuw draait (wel even een extra spatie toevoegen o.i.d.) is dit getal anders ;)
 // ---- Verwachte uitkomst: Bijbehorende zin afhankelijk van de uitkomst
+
+for(i=0; i<6; i++){
+    console.log(Math.random());
+}
